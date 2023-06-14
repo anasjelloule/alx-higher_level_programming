e29ce99559de92fc147e8291508c0d20c8455f40
Anas jelloul <anasjelloule@gmail.com>
2023-06-14 16:06:52 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        if(matrix):
-                return ([list(map(lambda ac: ac * ac, rw)) for rw in matrix])
+     if not matrix:
+        print()
+     return ([list(map(lambda ac: ac * ac, rw)) for rw in matrix])
