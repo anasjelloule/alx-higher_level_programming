529dbc8c4ec513cb3d0d9a3b3c0408449fdfeff0
anasjelloule <anasjelloule@gmail.com>
2023-06-14 08:26:58 -0700

    last

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-     return(list(map(lambda ac: list(map(lambda ax: ax ** 2, ac)), matrix)))
+    return(list(map(lambda ac: list(map(lambda ax: ax ** 2, ac)), matrix)))
