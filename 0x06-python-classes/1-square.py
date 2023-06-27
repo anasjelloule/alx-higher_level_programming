017effaaa1af35c8a9fcf54537936a9b7928ba0a
anasjelloule <anasjelloule@gmail.com>
2023-06-27 12:50:13 -0700

    class 25

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """
+    square class object
+    """
+    def __init__(self, size):
+        self.__size = size
+    """
+    init square object with parameter
+    args:
+        __size(int): size square
+    """
