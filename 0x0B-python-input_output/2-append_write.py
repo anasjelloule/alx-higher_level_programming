ad8237d8b4ffc7add77cbf15556478fab68dcec1
anasjelloule <anasjelloule@gmail.com>
2023-07-11 12:56:30 -0700

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-# ANAS Jelloul
-"""append to fil creates i not"""
+# Anas Jelloul
+"""Impliment file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """Return number of characters."""
-    with open(filename, mode="a+", encoding='utf-8') as f:
-        return(f.write(text))
+    """Insert a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): name_file.
+        text (str): string insert.
+    Returns:
+        Number characters Inserted.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
