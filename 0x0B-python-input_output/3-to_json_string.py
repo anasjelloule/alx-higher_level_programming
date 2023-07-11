140cef19371e255f397f48d5277005e079659b75
anasjelloule <anasjelloule@gmail.com>
2023-07-11 11:59:27 -0700

    task 5

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# ANAS Jelloul
+"""string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns string JSON  object."""
+    return json.dumps(my_obj)
