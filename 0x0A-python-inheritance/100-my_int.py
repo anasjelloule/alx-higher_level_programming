764d0cb511de38ad76971d1693058a9e6200b423
anasjelloule <anasjelloule@gmail.com>
2023-07-11 13:42:21 -0700

    task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Anas Jelloul
+"""Impliment class int."""
+
+
+class MyInt(int):
+    """Int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
+    
