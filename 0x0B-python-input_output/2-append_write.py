a753545d9788d323596566c4ec44f1da0d49d8ff
anasjelloule <anasjelloule@gmail.com>
2023-07-11 12:02:40 -0700

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+#ANAS Jelloul
 """append to fil creates i not"""
 
 
 def append_write(filename="", text=""):
-    """append to file or creates i not"""
+    """append to file or creates i not
+    Args:
+        filename (str): name of the file to append.
+        text (str): string to append in the file.
+    Returns:
+       number of characters.
+    """
     with open(filename, mode="a+", encoding='utf-8') as f:
         return(f.write(text))
