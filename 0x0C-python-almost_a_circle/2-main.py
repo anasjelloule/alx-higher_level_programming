63c0d0930c100ee352de9be65f9e7c237d24c7fc
anasjelloule <anasjelloule@gmail.com>
2023-07-20 02:32:51 -0700

    Task All rectangle V3

--- 0x0C-python-almost_a_circle/2-main.py
+++ 0x0C-python-almost_a_circle/2-main.py
@@ -11,6 +11,17 @@ if __name__ == "__main__":
 
     try:
         r = Rectangle(10, 2)
-        r.width(-10)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
