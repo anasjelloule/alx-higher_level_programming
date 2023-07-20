898eb0c4500ef2bbf77a9480afce5a4e21adae53
anasjelloule <anasjelloule@gmail.com>
2023-07-20 01:36:59 -0700

    Task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 # Anas Jelloul
 """Impliment a base model class."""
+
+
+class Base:
+    """Class base model.
+
+    Represents the "base" for all Inheritence classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): Number.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Init a new Base.
+
+        Args:
+            id (int): Identity new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
